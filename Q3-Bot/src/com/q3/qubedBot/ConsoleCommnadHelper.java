796a8d156d1336948e0708726414c154be860f71
100chilly <Quinten.Bidoul@gmail.com>
2015-09-24 20:33:17 -0400

    Added channel. See TODO in file.

--- Q3-Bot/src/com/q3/qubedBot/ConsoleCommnadHelper.java
+++ Q3-Bot/src/com/q3/qubedBot/ConsoleCommnadHelper.java
@@ -1,29 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.q3.qubedBot;
 
 
 public class ConsoleCommnadHelper {
-
+    
     /**
      *
      * @param command
-     * @return 
+     * @return
      */
     public void runConsoleCommand(String command) {
-    
-	  switch(command){
-	  case("say"):
-		  //TODO read server + channel and print message to that channel or entire server
-		  break;
-	  case("join"):
-		  //TODO ask series of questions to either join a new server or channel on a server already connected to.
-		  break;
-	  default:
-		  System.out.println("Unknown Command");
-	  }
-  }
+        
+        switch(command){
+            case("say"):
+                //TODO read server + channel and print message to that channel or entire server
+                break;
+            case("join"):
+                //TODO ask series of questions to either join a new server or channel on a server already connected to.
+                break;
+            case("channel"):
+                //TODO When user enters this command, it ask them what channel/room they want to switch to on an already connected network.
+                break;
+            default:
+                System.out.println("Unknown Command");
+        }
+    }
 }
